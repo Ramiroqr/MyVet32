e6f3e822a2dd230d3fdb52c396166544d2130741
Ramiroqr <ramiroquisper@gmail.com>
2020-05-15 07:03:11 -0500

    All owner

---
+++ MyVet.Web/Helpers/CombosHelper.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MyVet.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Helpers
+{
+    public class CombosHelper : ICombosHelper
+    {
+        private readonly DataContext _dataContext;
+
+        public CombosHelper(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public IEnumerable<SelectListItem> GetComboPetTypes()
+        {
+            var list = _dataContext.PetTypes.Select(pt => new SelectListItem
+            {
+                Text = pt.Name,
+                Value = $"{pt.Id}"
+            })
+                .OrderBy(pt => pt.Text)
+                .ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Selec a pet Type...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+        public IEnumerable<SelectListItem> GetComboServiceTypes()
+        {
+            var list = _dataContext.ServiceTypes.Select(p => new SelectListItem
+            {
+                Text = p.Name,
+                Value = p.Id.ToString()
+            }).OrderBy(p => p.Text).ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "(Select a service type...)",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+    }
+}
